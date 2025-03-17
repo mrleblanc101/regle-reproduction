8fe5d119d8fb5699ddf2f9615cbb0fa4be68f7c2
mrleblanc101 <mrsebastienleblanc@icloud.com>
2025-03-17 14:28:19 -0400

    repro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,33 +2,41 @@
 import Drawer from '@/components/Drawer.vue'
 import JSONViewer from '@/components/JSONViewer.vue'
 import { useRegle } from '@regle/core'
-import { applyIf, minLength, required } from '@regle/rules'
-import { ref } from 'vue'
+import { required } from '@regle/rules'
+import { ref, type Ref } from 'vue'
 
-const condition = ref(true)
+const form: Ref<{
+  frequency: number | null
+}> = ref({
+  frequency: null
+})
 
 const { r$ } = useRegle(
-  { name: '' },
-  {
-    name: {
-      required: applyIf(condition, required),
-      minLength: applyIf(condition, minLength(3)),
-    },
-  },
-)
+  form,
+  { frequency: { required } }
+);
+
+const onSubmit = async () => {
+  const { result } = await r$.$validate();
+    if (result) {
+
+
+      alert(form.value.frequency);
+    }
+};
 </script>
 
 <template>
   <div class="px-6 text-gray-900 antialiased">
     <div class="mx-auto max-w-xl py-12 md:max-w-4xl">
       <h2 class="text-2xl mb-8">Regle reproduction</h2>
-      <div class="flex flex-col">
+      <form class="flex flex-col" @submit.prevent="onSubmit">
         <label>Your name</label>
-        <input class="border p-2 rounded" v-model="r$.$value.name" placeholder="Victor Regle" />
-        <ul class="text-red-400 text-sm mt-1" v-if="r$.$errors.name.length">
-          <li v-for="error of r$.$errors.name" :key="error">{{ error }}</li>
+        <input class="border p-2 rounded" v-model="form.frequency" placeholder="Victor Regle" />
+        <ul class="text-red-400 text-sm mt-1" v-if="r$.$errors.frequency.length">
+          <li v-for="error of r$.$errors.frequency" :key="error">{{ error }}</li>
         </ul>
-      </div>
+      </form>
       <Drawer>
         <JSONViewer :data="r$" />
       </Drawer>
